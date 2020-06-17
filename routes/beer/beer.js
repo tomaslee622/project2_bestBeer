da69eab6d0b72f9a229afb3b7c4f6e0539db7bf0
ch-oi <gmu1233@gmail.com>
2020-06-17 13:08:53 +0800

    fiajds

--- routes/beer/beer.js
+++ routes/beer/beer.js
@@ -15,8 +15,13 @@ module.exports = (express) => {
     };
 
     const getReviews = (id) => {
-        let query = knex('reviews').select().where({ beer_id: id });
-        return query.then((data) => data);
+        let reviewData;
+        let query = knex('reviews')
+            .join('users', 'reviews.user_id', '=', 'users.id')
+            .select('users.first_name', 'reviews.content');
+        return query.then((data) => {
+            return data;
+        });
     };
 
     router.get('/:id', async(req, res) => {
@@ -30,11 +35,7 @@ module.exports = (express) => {
     router.get('/', async(req, res) => {
         let data = await getAllBeers();
         // res.send(data);
-<<<<<<< HEAD
-        res.render('test', { beer: data });
-=======
-        res.render('menu_page',{beer: data});
->>>>>>> 39667a262881e03fd024d630a15414d471f7bd75
+        res.render('menu_page', { beer: data });
     });
 
     return router;
