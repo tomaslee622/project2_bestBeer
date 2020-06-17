9adce30f09652bdcda7ef9ce3f38724e18ca4045
ch-oi <gmu1233@gmail.com>
2020-06-17 20:43:43 +0800

    All authentication done

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,7 @@
 const passport = require('passport');
 const bcrypt = require('bcrypt');
+const knexConfig = require('../knexfile')['development'];
+const knex = require('knex')(knexConfig);
 
 module.exports = (express) => {
     const router = express.Router();
@@ -39,24 +41,51 @@ module.exports = (express) => {
     });
 
     router.post('/register', async(req, res) => {
-        try {
-            const hashedPassword = await bcrypt.hash(req.body.password, 10);
-            user.push({
-                email: req.body.username,
-                password: hashedPassword,
-            });
-            res.redirect('/login');
-        } catch {
-            res.redirect('/error');
-        }
+        // try {
+        // console.log(req.body);
+        let check = knex('users').select().where('email', '=', req.body.email);
+        check.then((data) => {
+            if (data.length >= 1) {
+                console.log('Email exists.');
+                res.send('Email exists');
+            }
+        });
 
-        res.redirect('/login');
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+        console.log(req.body.firstName);
+        console.log(hashedPassword);
+
+        let query = await knex('users').insert({
+            first_name: req.body.firstName,
+            last_name: req.body.lastName,
+            email: req.body.email,
+            password: hashedPassword,
+        });
+
+        query.then((data) => {
+            console.log('User registered');
+            console.log(data);
+            res.redirect('/login');
+        });
+        // res.redirect('/login');
+        // } catch {
+        //     res.redirect('/error');
+        // }
     });
 
     router.get('/logout', (req, res) => {
         req.logOut();
         res.redirect('/');
     });
+
+    router.post(
+        '/login',
+        passport.authenticate('local-login', {
+            successRedirect: '/',
+            failureRedirect: '/login',
+        })
+    );
 
     return router;
 };
