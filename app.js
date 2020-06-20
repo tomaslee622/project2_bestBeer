aea42a76f14e7e6c002c3f215cca2ef86d22849f
tomaslee622 <t.lee.cwl@gmail.com>
2020-06-20 18:24:02 +0800

    changes, animation> company_info&promote,homepage_view&logged_in, page_header

--- app.js
+++ app.js
@@ -90,6 +90,6 @@ const knex = require('knex')(knexConfig);
 //     console.log(data);
 // });
 
-app.listen(3001, () => {
+app.listen(3000, () => {
     console.log(`App is listening to port 3000`);
 });
