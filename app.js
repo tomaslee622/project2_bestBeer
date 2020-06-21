a7fbb04e41777746cba86ac8a2536826f393a418
Ch-oi <gmu1233@gmail.com>
2020-06-22 04:43:51 +0800

    Real final update

--- app.js
+++ app.js
@@ -81,10 +81,10 @@ app.get('/chart', (req, res) => {
 const knexConfig = require('./knexfile')['development'];
 const knex = require('knex')(knexConfig);
 
-// let query = knex('purchase').select();
-// query.then((data) => {
-//     console.log(data);
-// });
+let query = knex('purchase').select();
+query.then((data) => {
+    console.log(data);
+});
 
 // let query = knex('purchase')
 //     .join('beers', 'beers.id', 'purchase.beer_id')
